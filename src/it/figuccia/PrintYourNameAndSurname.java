8627243ec4c77b27e3d6df51856bd380d3c11a7b
Keicabba <peppino_cbr@hotmail.com>
2023-01-27 02:50:26 +0100

    Initial commit

---
+++ src/it/figuccia/PrintYourNameAndSurname.java
@@ -0,0 +1,9 @@
+package it.figuccia;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.println("Pinco");
+        System.out.println("Pallino");
+    }
+}
+//ribadisco. aver fatto oggetti, condizioni, metodi ecc. prima di questo... non ha senso.
